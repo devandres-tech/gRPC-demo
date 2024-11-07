0c871d2c2008c3ef87a5e95c3257fef183232a53
andres alcocer <devandres.tech@gmail.com>
2024-11-06 21:12:45 -0500

    created client side communication

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using GrpcDemo;
+
+var input = new HelloRequest { Name = "Andres" };
+
+var channel = GrpcChannel.ForAddress("http://localhost:5151");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(input);
+Console.WriteLine(reply.Message);
 Console.ReadLine();
